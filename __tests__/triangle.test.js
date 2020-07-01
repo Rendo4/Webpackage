58ec67f360e99e5c3ca4587c54d10cce13f726ef
Ryan Rendon <Rendon.S.Ryan@gmail.com>
2020-06-30 21:05:39 -0700

    added bootstrap and Jquery to webpack and started jest testing code

---
+++ __tests__/triangle.test.js
@@ -0,0 +1,10 @@
+import { pingpong } from './../src/ping-pong.js';
+
+describe('pingPong', () => {
+
+  test('should return array of numbers replacing them with ping, pong, or ping-pong', () => {
+    let output = []
+    expect(output).toEqual(output);
+
+  });
+});
